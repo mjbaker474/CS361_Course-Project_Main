f02e423a738fd2e17db29795f0085d1da6894bad
mjbaker474 <mjbaker474@gmail.com>
2025-02-08 12:51:17 -0600

    Title output works

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+# Name:  Matthew Baker
+# OSU Email:  bakerma2@oregonstate.edu
+# Course: CS361 - Software Engineering 1
+# Assignment:  Course Project
 #
+# File: Main
+# Description: Acts as the primary user interface and client for microservices.
+
+import pyfiglet
+import os
+import ctypes
+import random
+import time
+from pyautogui import hotkey
+from pyfiglet import DEFAULT_FONT
+
+
+
+def main():
+
+    print_title()
+    print_main_menu()
+    message_box()
+
+
+def process_input():
+    pass
+
+def welcome_screen()-> None:
+    pass
+
+def help_screen():
+    pass
+
+def message_box():
+    ctypes.windll.user32.MessageBoxW(0, "TEst", "Titles Test", 1)
+
+def print_title(font_number: int = 0) -> None:
+    """Prints out the title in either default font or font based off of random number passed to the function."""
+    fonts = [DEFAULT_FONT, 'slant', 'alphabet', '3-d', '3x5', '5lineoblique', 'banner3-D', 'doh', 'isometric1',
+             'letters', 'dotmatrix', 'bubble', 'bulbhead', 'digital']
+    title = pyfiglet.figlet_format("The Randomizer", font=fonts[font_number], width = 256)
+    print("\n", title, '_'*256, sep='')
+
+def print_main_menu():
+    """Prints out the main menu"""
+    print("", "Welcome to the randomizer! Select an option from the menu below, press 'H' for help, or 'Q' to quit.",
+          "", "1. Option 1", "2. Option 2", "3. Option 3", "4. Option 4", sep = "\n")
+
+if __name__ == '__main__':
+    main()
