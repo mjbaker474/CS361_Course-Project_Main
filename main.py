0a30c798e423653df8a0e542c5b20740652fb177
mjbaker474 <mjbaker474@gmail.com>
2025-03-01 18:28:33 -0600

    Cleaned up formatting and documentation.

--- main.py
+++ main.py
@@ -13,15 +13,14 @@ from pyautogui import hotkey
 
 
 class Randomizer:
+    """A randomizer object is an instance of the Randomizer app."""
     def __init__(self):
         random.seed()
-        self.socket_a = self.connect_socket(2025) # Random activity service.
-        self.socket_b = self.connect_socket(2026) # Random fact service.
-        self.socket_c = self.connect_socket(2027) # Random inspirational quote service.
-        self.socket_d = self.connect_socket(2028)
+        self.socket_a = connect_socket(2025) # Random activity service.
+        self.socket_b = connect_socket(2026) # Random fact service.
+        self.socket_c = connect_socket(2027) # Random inspirational quote service.
+        self.socket_d = connect_socket(2028)
         self.title_font = pyfiglet.DEFAULT_FONT
-
-
 
     def print_title(self) -> None:
         """Prints out the title in either default font or a randomly selected font."""
@@ -29,7 +28,7 @@ class Randomizer:
         print("\n", title, '_' * 256, sep='')
 
     def main_menu(self) -> None:
-        self.clear()
+        clear()
         self.print_title()
         print("", "Welcome to the randomizer! Select an option from the menu below, press 'H' for help, or 'Q' to quit.",
               "", "1. Generate a random number", "2. Suggest a random activity", "3. Receive random inspiration",
@@ -60,7 +59,7 @@ class Randomizer:
     def generate_random(self) -> None:
         """Outputs a random number to the screen."""
         rand_num = str(random.randint(0, 1000))
-        self.clear()
+        clear()
         self.print_title()
         print("\n", f"Your random number is: {rand_num}", "\n" * 3,"Enter Y to generate another random number or any other key to return." )
         user_input = input("")
@@ -74,7 +73,7 @@ class Randomizer:
         rand = str(random.randint(0, 39))
         self.socket_a.send_string(rand)
         activity = self.socket_a.recv().decode()
-        self.clear()
+        clear()
         self.print_title()
         print("\n", f"You should try: {activity}", "\n" * 3, f"Enter Y to receive another suggestions or any other key "
                                         f"to return.")
@@ -84,9 +83,9 @@ class Randomizer:
         else:
             self.main_menu()
 
-    def random_quote_menu(self):
+    def random_quote_menu(self) -> None:
         """Prints the random quote sub menu."""
-        self.clear()
+        clear()
         self.print_title()
         print("", "Select an option from the menu below, or any other key to return to the main menu.",
               "", "1. Inspiration from an artist", "2. Inspiration from a historical figure",
@@ -104,7 +103,7 @@ class Randomizer:
         options = {"1": "Artist ", "2": "Historical Figure ", "3": "Innovator ", "4": ""}
         self.socket_c.send_string(option)
         quote = self.socket_c.recv().decode()
-        self.clear()
+        clear()
         self.print_title()
         print("\n", quote, "\n" * 3, f"Enter Y to generate another random inspirational {options[option]}quote, R to "
                                      f"return to previous menu, or any other key to return.")
@@ -117,9 +116,9 @@ class Randomizer:
             case _:
                 self.main_menu()
 
-    def random_fact_menu(self):
+    def random_fact_menu(self) -> None:
         """Prints the random fact sub menu."""
-        self.clear()
+        clear()
         self.print_title()
         print("", "Select an option from the menu below, or any other key to return to the main menu.",
               "", "1. Learn about science", "2. Learn about history",
@@ -132,12 +131,12 @@ class Randomizer:
                 case _:
                     self.main_menu()
 
-    def random_fact(self, option) -> None:
+    def random_fact(self, option: str) -> None:
         """Outputs a random fact to the screen."""
         options = {"1": "Science ", "2": "History ", "3": "Pop Culture ", "4": ""}
         self.socket_b.send_string(option)
         fact = self.socket_b.recv().decode()
-        self.clear()
+        clear()
         self.print_title()
         print("\n", fact, "\n" * 3, f"Enter Y to generate another random {options[option]}fact, R to return to previous"
                     f" menu, or any other key to return.")
@@ -150,11 +149,11 @@ class Randomizer:
             case _:
                 self.main_menu()
 
-    def set_title_font(self, font) -> None:
+    def set_title_font(self, font: str) -> None:
         """Updates the title font."""
         self.title_font = font
 
-    def randomize_font(self):
+    def randomize_font(self) -> None:
         """Randomly changes the title font."""
         fonts = [pyfiglet.DEFAULT_FONT, 'slant', 'alphabet', '3-d', '3x5', '5lineoblique', 'banner3-D', 'doh',
                  'isometric1','letters', 'dotmatrix', 'bubble', 'bulbhead', 'digital']
@@ -168,7 +167,7 @@ class Randomizer:
 
     def help_screen(self) -> None:
         """Displays help screen to user."""
-        self.clear()
+        clear()
         self.print_title()
         print("",
               "Welcome to the Randomizer app! This app is designed to increase spontaneity in your life by providing",
@@ -184,7 +183,7 @@ class Randomizer:
 
     def quit_screen(self) -> None:
         """Displays quit confirmation screen to user."""
-        self.clear()
+        clear()
         self.print_title()
         print('\n', "Are you sure you want to quit? Press any key to return to the main menu, or 'Q' to quit.")
         user_input = input("")
@@ -194,16 +193,16 @@ class Randomizer:
             case _:
                 self.main_menu()
 
-    def connect_socket(self, port: int):
-        """Creates a ZeroMQ TCP connection to communicate with microservices."""
-        context = zmq.Context()
-        socket = context.socket(zmq.REQ)
-        socket.connect(f"tcp://localhost:{port}")
-        return socket
+def connect_socket(port: int) -> zmq.Socket:
+    """Creates a ZeroMQ TCP connection to communicate with microservices."""
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f"tcp://localhost:{port}")
+    return socket
 
-    def clear(self) -> None:
-        """Clears the screen."""
-        hotkey('ctrl', 'shift', ';')
+def clear() -> None:
+    """Clears the screen."""
+    hotkey('ctrl', 'shift', ';')
 
 if __name__ == '__main__':
     app = Randomizer()
